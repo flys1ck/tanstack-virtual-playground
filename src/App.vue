08a61f6709fcd31c8a764fa1ae68d64075db977c
Gregor Zweig <gregor.zweig@memsource.com>
2023-11-02 19:01:54 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="max-w-2xl mx-auto">
+    <BeerList />
+  </main>
+</template>
+
+<script setup lang="ts">
+import BeerList from "@/components/BeerList.vue";
+
+/**
+ * Your task is to implement a virtualized list of beers.
+ *
+ * 1. Use the `BeerListItem` component to render each beer. Start with the sample data in `./data/beers.ts`.
+ * 2. Fetch beers from the BeersAPI (https://api.punkapi.com/v2/beers). You can use the `fetch` API or any other library.
+ * 3. Use the `useVirtualizer` composable to virtualize the list. See the example in `./examples/RowVirtualizerExample.vue`.
+ * 4. Load more beers, when the user scrolls to the bottom of the list.
+ */
+</script>
---
+++ src/components/BeerCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex justify-between gap-x-6 py-5">
+    <div class="flex min-w-0 gap-x-4 items-center">
+      <img
+        class="h-36 w-36 flex-none rounded bg-gray-50 object-contain border"
+        :src="beer.image_url"
+        :alt="beer.name"
+      />
+      <div class="min-w-0 flex-auto">
+        <p class="text-xl font-semibold leading-6 text-gray-900">
+          {{ beer.name }}
+        </p>
+        <p class="mt-1 text-gray-500 line-clamp-3">{{ beer.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import beers from "../data/beers";
+
+defineProps<{
+  beer: (typeof beers)[number];
+}>();
+</script>
